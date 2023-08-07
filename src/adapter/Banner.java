a73630242a5c4f88c15b8ea98516f18cc066ae0a
wangzhenxiao <2022613398@qq.com>
2023-08-07 15:34:38 +0800

    adapter design pattern done

---
+++ src/adapter/Banner.java
@@ -0,0 +1,24 @@
+package adapter;
+
+/**
+ * @program: DesignPatternStudy
+ * @description:
+ * @author: Havad
+ * @create: 2023-08-07 15:16
+ **/
+
+public class Banner {
+    private String string;
+
+    public Banner(String string) {
+        this.string = string;
+    }
+
+    public void showWithParen() {
+        System.out.println("(" + string + ")");
+    }
+
+    public void showWithAster() {
+        System.out.println("*" + string + "*");
+    }
+}
