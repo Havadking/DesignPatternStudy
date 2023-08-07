fd84e555fa7e094b70732cfd7a3d94b2d35d575b
wangzhenxiao <2022613398@qq.com>
2023-08-07 15:06:06 +0800

    iterator design pattern done

---
+++ src/iterator/Aggregate.java
@@ -0,0 +1,13 @@
+package iterator;
+
+
+/**
+ * @program: DesignPatternStudy
+ * @description:
+ * @author: Havad
+ * @create: 2023-08-07 14:39
+ **/
+
+public interface Aggregate {
+    Iterator iterator();
+}
---
+++ src/iterator/BookShelf.java
@@ -0,0 +1,34 @@
+package iterator;
+
+/**
+ * @program: DesignPatternStudy
+ * @description:
+ * @author: Havad
+ * @create: 2023-08-07 14:42
+ **/
+
+public class BookShelf implements Aggregate {
+    private Book[] books;
+    private int last = 0;
+
+    public BookShelf(int maxSize) {
+        this.books = new Book[maxSize];
+    }
+
+    public Book getBookAt(int index) {
+        return books[index];
+    }
+
+    public int getLength() {
+        return last;
+    }
+
+    public void appendBook(Book book) {
+        this.books[last] = book;
+        last++;
+    }
+    @Override
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
